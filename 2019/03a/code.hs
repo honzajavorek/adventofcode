dc56b82426bde1f8212b2361ef19074a52e63ebe
Honza Javorek <mail@honzajavorek.cz>
2019-12-06 11:02:31 +0100

    prepare fold

--- 2019/03a/code.hs
+++ 2019/03a/code.hs
@@ -1,7 +1,7 @@
 type Distance = Int
 
 data Direction = U | R | D | L deriving (Eq, Show)
-data Segment = Segment { direction :: Direction
+data Segment = Segment { dir :: Direction
                        , steps :: Int
                        } deriving (Eq, Show)
 data Coord = Coord { x :: Int
@@ -9,8 +9,8 @@ data Coord = Coord { x :: Int
                    } deriving (Eq, Show)
 
 
-main' = interact $ show . minimum . distances . crossings . (map coords) . (map parse) . lines
-main = interact $ show . (map parse) . lines
+main' = interact $ show . minimum . distances . crossings . (map toCoords) . (map parse) . lines
+main = interact $ show . (map toCoords) . (map parse) . lines
 
 
 parse :: String -> [Segment]
@@ -18,17 +18,21 @@ parse code = map parseSegment $ words [if c == ',' then ' ' else c | c <- code]
 
 parseSegment :: String -> Segment
 parseSegment (char:digits) = case char of
-    'U' -> Segment {direction = U, steps = steps}
-    'R' -> Segment {direction = R, steps = steps}
-    'D' -> Segment {direction = D, steps = steps}
-    'L' -> Segment {direction = L, steps = steps}
+    'U' -> Segment {dir = U, steps = steps}
+    'R' -> Segment {dir = R, steps = steps}
+    'D' -> Segment {dir = D, steps = steps}
+    'L' -> Segment {dir = L, steps = steps}
     where steps = read digits :: Int
 
-coords :: [Segment] -> [Coord]
-coords wireAsSegments = [(Coord {x = 0, y = 2})]
+toCoords :: [Segment] -> [Coord]
+toCoords = foldl foldSegment [Coord {x = 0, y = 0}]
+
+foldSegment :: [Coord] -> Segment -> [Coord]
+foldSegment (lastCoord@(Coord { x = x, y = y }):coords) (Segment { dir = dir, steps = steps })
+    | otherwise = lastCoord:coords
 
 crossings :: [[Coord]] -> [Coord]
-crossings wiresAsCoords = [(Coord {x = 0, y = 2})]
+crossings coords = [(Coord {x = 0, y = 2})]
 
 distances :: [Coord] -> [Distance]
 distances coords = [2]
