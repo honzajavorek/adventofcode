e5bf685b24e3395227b4b3c433e5638bdb618826
Honza Javorek <mail@honzajavorek.cz>
2019-12-06 12:52:53 +0100

    use damn sets

--- 2019/03a/code.hs
+++ 2019/03a/code.hs
@@ -1,10 +1,12 @@
-data Direction = U | R | D | L deriving (Eq, Show)
-data Segment = Segment { dir :: Direction
+import qualified Data.Set as Set
+
+
+data Segment = Segment { dir :: Char
                        , steps :: Int
                        } deriving (Eq, Show)
 data Coord = Coord { x :: Int
                    , y :: Int
-                   } deriving (Eq, Show)
+                   } deriving (Eq, Show, Ord)
 
 
 main = interact $ show . minDistance . distances . crossings . (map toCoords) . (map parse) . lines
@@ -14,22 +16,22 @@ parse :: String -> [Segment]
 parse code = map parseSegment $ words [if c == ',' then ' ' else c | c <- code]
 
 parseSegment :: String -> Segment
-parseSegment (char:digits) = case char of
-    'U' -> Segment {dir = U, steps = steps}
-    'R' -> Segment {dir = R, steps = steps}
-    'D' -> Segment {dir = D, steps = steps}
-    'L' -> Segment {dir = L, steps = steps}
+parseSegment (dir:digits) = Segment {dir = dir, steps = steps}
     where steps = read digits :: Int
 
-toCoords :: [Segment] -> [Coord]
-toCoords = foldl foldSegment [Coord {x = 0, y = 0}]
+toCoords :: [Segment] -> Set.Set Coord
+toCoords segments = snd $ foldl foldSegment (initCoord, (Set.singleton initCoord)) segments
+    where initCoord = Coord {x = 0, y = 0}
 
-foldSegment :: [Coord] -> Segment -> [Coord]
-foldSegment (prevCoord:coords) (Segment { dir = dir, steps = steps })
-    | dir == U = (generateCoords keep (+) steps prevCoord) ++ coords
-    | dir == R = (generateCoords (+) keep steps prevCoord) ++ coords
-    | dir == D = (generateCoords keep (-) steps prevCoord) ++ coords
-    | dir == L = (generateCoords (-) keep steps prevCoord) ++ coords
+foldSegment :: (Coord, Set.Set Coord) -> Segment -> (Coord, Set.Set Coord)
+foldSegment (prevCoord, coords) (Segment { dir = dir, steps = steps }) = (lastNewCoord, mergedCoords)
+    where
+        (newCoords@(lastNewCoord:_)) = case dir of
+            'U' -> generateCoords keep (+) steps prevCoord
+            'R' -> generateCoords (+) keep steps prevCoord
+            'D' -> generateCoords keep (-) steps prevCoord
+            'L' -> generateCoords (-) keep steps prevCoord
+        mergedCoords = coords `Set.union` (Set.fromList newCoords)
 
 generateCoords :: (Int -> Int -> Int) -> (Int -> Int -> Int) -> Int -> Coord -> [Coord]
 generateCoords opX opY steps (Coord { x = prevX, y = prevY }) =
@@ -40,11 +42,11 @@ generateCoords opX opY steps (Coord { x = prevX, y = prevY }) =
 keep :: Int -> Int -> Int
 keep coord step = coord
 
-crossings :: [[Coord]] -> [Coord]
-crossings [coords1,coords2] = [c1 | c1 <- coords1, c2 <- coords2, c1 == c2]
+crossings :: [Set.Set Coord] -> Set.Set Coord
+crossings [coords1,coords2] = coords1 `Set.intersection` coords2
 
-distances :: [Coord] -> [Int]
-distances coords = [(abs x) + (abs y) | (Coord { x = x, y = y }) <- coords]
+distances :: Set.Set Coord -> [Int]
+distances coords = [(abs x) + (abs y) | (Coord { x = x, y = y }) <- (Set.toList coords)]
 
 minDistance :: [Int] -> Int
 minDistance distances = minimum [d | d <- distances, d > 0]
